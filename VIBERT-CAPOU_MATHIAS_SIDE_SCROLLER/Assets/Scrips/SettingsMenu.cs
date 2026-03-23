612fc67654c36b75e319617789dd305d6a115128
Mathvib77 <m.vibertcapou@bdx.ecolescreatives.com>
2026-03-23 18:01:04 +0100

    updates on resolution settings

--- VIBERT-CAPOU_MATHIAS_SIDE_SCROLLER/Assets/Scrips/SettingsMenu.cs
+++ VIBERT-CAPOU_MATHIAS_SIDE_SCROLLER/Assets/Scrips/SettingsMenu.cs
@@ -19,13 +19,21 @@ public class SettingsMenu : MonoBehaviour
 
         List<string> options = new List<string>();
 
+        int currentResolutionIndex = 0;
         for (int i = 0; i < resolutions.Length; i++)
         {
             string option = resolutions[i].width + " x " + resolutions[i].height;
             options.Add(option);
+
+            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
         }
 
         resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
     }
     public void SetVolume(float volume)
     { 
